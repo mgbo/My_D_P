9d2c63a410dac0916d843d7c2845a45d8a9ac479
mgbo <mgbo433@gmail.com>
2020-08-30 11:21:00 +0630

    trying create pdf from html

---
+++ django_pdf/customers/models.py
@@ -0,0 +1,11 @@
+
+from django.db import models
+
+class Customer(models.Model):
+	name = models.CharField(max_length=200)
+	description = models.TextField()
+	updated = models.DateTimeField(auto_now=True)
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.name)
